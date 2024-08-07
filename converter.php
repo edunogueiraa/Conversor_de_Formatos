e7c450d8ed7c20596e9b1f4bf3de50b3b25d1c9a
edunogueiraa <eduardonogueira105@gmail.com>
2024-08-06 21:48:35 -0300

    Fazendo2

--- converter.php
+++ converter.php
@@ -11,5 +11,9 @@ if (sizeof($argv) != 2) {
 }
 
 if (pathinfo($argv[1], PATHINFO_EXTENSION) == 'json') {
-
+    // Código para converter JSON para XML
+}elseif (pathinfo($argv[1], PATHINFO_EXTENSION) == 'xml') {
+    // Código para converter XML para JSON
+} else {
+    echo "Este arquivo não é xml e nem json";
 }
