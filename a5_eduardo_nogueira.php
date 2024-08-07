2d02c6970e7a76eedf0b2c89b4e12ea57871b158
edunogueiraa <eduardonogueira105@gmail.com>
2024-08-06 21:05:22 -0300

    Fazendo

--- a5_eduardo_nogueira.php
+++ a5_eduardo_nogueira.php
@@ -1,1 +1,15 @@
 <?php
+
+/*Integrantes
+    Eduardo Nogueira da Silva
+    Pedro Henrique Dutra
+*/
+
+if (sizeof($argv) != 2) {
+    echo "Uso: php converter.php (arquivo.json | arquivo.xml)\n";
+    exit(1);
+}
+
+if (pathinfo($argv[1], PATHINFO_EXTENSION) == 'json') {
+
+}
